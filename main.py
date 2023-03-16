77c2e765e198fe7e1add6fc06d0cd196d989c0fb
walesoluade <walesoluade@gmail.com>
2023-03-16 10:57:19 -0600

    added the main module

---
+++ main.py
@@ -0,0 +1,9 @@
+from util import get_database_conn
+from extract_football_data import  extract_data, transform_data, load_data
+
+def main():
+    #extract_data()
+    #transform_data()
+    load_data()
+
+main()
